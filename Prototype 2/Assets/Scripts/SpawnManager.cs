00f2c5ee92f2e07f35c20fedafea6f02a5e689d0
Phoenix Schneider <10908806@uvu.edu>
2021-10-26 21:42:30 -0600

    Finished adding collisions

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,24 +7,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPreFabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown (KeyCode.S))
-        {
-            // Randomly generate animal index and spawn position 
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPreFabs.Length);
-
-            Instantiate(animalPreFabs[animalIndex], spawnPos,
-            animalPreFabs[animalIndex].transform.rotation);
-        }
+     
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPreFabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPreFabs[animalIndex], spawnPos,
+        animalPreFabs[animalIndex].transform.rotation);
     }
 }
